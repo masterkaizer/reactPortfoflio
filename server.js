7a9beb78b6bae7b17f91607a497e163229be79aa
Your Name <brandon.sellam@gmail.com>
2020-06-07 03:45:47 +0200

    opl

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-app.use(express.static("client/build"));
+
 
 
 // Send every request to the React app
